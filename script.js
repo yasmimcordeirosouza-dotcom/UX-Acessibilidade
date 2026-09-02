f4032ab968640394e4f62d121b46bc5514021e45
yasmimcordeirosouza-dotcom <yasmim.cordeiro.souza@escola.pr.gov.br>
2026-09-02 01:12:20 +0000

    commit

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// CÓDIGO DO MODAL
 let btnAjuda = document.querySelector(".botao-ajuda");
 let btnFechar = document.querySelector(".botao-fechar");
 let modal = document.querySelector(".modal-fundo");
@@ -11,4 +12,68 @@ function abreModal() {
 
 function fechaModal() {
     modal.style.display = "none";
+}
+
+
+// TAMANHO DE FONTES
+let tamanhoFonteAtual = 16;
+const valorAdicionado = 2;
+const valorSubtraido = 2;
+
+let btnAumentaFonte = document.getElementById("btnAumentaTexto");
+let btnDiminuiFonte = document.getElementById("btnDiminuiTexto");
+
+btnAumentaFonte.addEventListener("click", aumentaFonte);
+btnDiminuiFonte.addEventListener("click", diminuiFonte);
+
+function aumentaFonte() {
+    tamanhoFonteAtual = tamanhoFonteAtual + valorAdicionado;
+    document.documentElement.style.fontSize = `${tamanhoFonteAtual}px`;
+}
+
+function diminuiFonte() {
+    tamanhoFonteAtual = tamanhoFonteAtual - valorSubtraido;
+    document.documentElement.style.fontSize = `${tamanhoFonteAtual}px`;
+}
+
+// LEITURA DE TELA
+
+let lendo = false;
+
+let btnLeitura = document.querySelector(".botao-leitura");
+
+btnLeitura.addEventListener("click", lerEmVozAlta);
+
+function lerEmVozAlta() {
+
+    // se já está lendo
+    if (lendo == true) {
+
+        // se estiver pausado 
+        if (speechSynthesis.paused == true){
+            // continua de onde parou
+            speechSynthesis.resume();
+        } else {
+            // pausa
+            speechSynthesis.pause();
+        }
+        return;
+    }
+
+    let conteudo = document.querySelector("main");
+    let texto = conteudo.innerText;
+
+    let fala = new SpeechSynthesisUtterance(texto);
+
+    fala.lang = "pt-BR";
+    fala.onend = finalizarLeitura;
+
+    lendo = true
+
+    speechSynthesis.cancel();
+    speechSynthesis.speak(fala);
+}
+
+function finalizarLeitura() {
+    lendo = false;
 }
